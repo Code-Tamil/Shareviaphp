78d6b0ffb511a235caa9907e7c520ba856ef42f7
Tharun Kumar <66469788+Code-Tamil@users.noreply.github.com>
2023-04-29 21:32:56 +0530

    Create Share.sh

---
+++ Share.sh
@@ -0,0 +1,5 @@
+sudo service apache2 start
+echo "This is Your IP Connect your Mobile with Hotspot/Wifi and got to browser in your phone and type this address:--"
+ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}'
+echo "PC-MOBILE  --> Copy the files you want to share to /var/www/html/shares then you could download it in your mobile without Internet and cables/Wires"
+echo "MOBILE-PC  --> Click send button in the browser and choose the file and upload You can find the uploaded file in /var/www/html/shares after complete upload(upload may take some time)"
