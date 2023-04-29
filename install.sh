722007983777e60db01543c149f1477599fc1dce
Tharun Kumar <66469788+Code-Tamil@users.noreply.github.com>
2023-04-29 22:09:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ cd Shareviaphp
 sudo apt update
 sudo apt install php apache2
 sudo cp -r * /var/www/html
-sudo mkdir /var/www/html/shares
 sudo chmod 777 /var/www/html/shares
 sudo chmod 777 Share.sh
 echo "Run 'sudo ./Shareviaphp/Share.sh'"
