4ab99496346f53be49a8cadb68de40a5710536e7
Tharun Kumar <66469788+Code-Tamil@users.noreply.github.com>
2023-04-29 21:58:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ git clone https://github.com/Code-Tamil/Shareviaphp.git
 cd Shareviaphp
 sudo apt update
 sudo apt install php apache2
-sudo mv * /var/www/html
+sudo cp * /var/www/html
 sudo mkdir /var/www/html/shares
 sudo chmod 777 /var/www/html/shares
 sudo chmod 777 Share.sh
